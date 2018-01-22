37939cd086058203f99c0305c05002234a5165ce
cherrygu <cherrygu1209@gmail.com>
2018-01-22 16:57:02 +0800

    Log in and Sign up
    Log in and sign up

--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -7,29 +7,52 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
-$this->title = 'Signup';
+$this->title = 'Sign up - GoRaisin';
 $this->params['breadcrumbs'][] = $this->title;
+frontend\assets\LoginAsset::register($this);
 ?>
 <div class="site-signup">
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div id="login-div">
+        <p class="login-notice">Have an account?<?= Html::a('Log in', ['site/login'],['class' => 'signup-a']) ?></p>
+        <hr />
+        <p class="form-title">Sign up</p>
+        <div class="row">
+            <div style="margin-left: 3%;margin-top: 9%;margin-right: 3%;">
+                <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
 
-    <p>Please fill out the following fields to signup:</p>
+                <?= $form->field($model, 'username')
+                    ->textInput(['autofocus' => true,'style' => 'width:380px','placeholder' => 'Username'])
+                    ->label(false)
+                ?>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
+                <?= $form->field($model, 'email')
+                    ->textInput(['style' => 'width:380px','placeholder' => 'Email'])
+                    ->label(false)
+                ?>
 
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+                <?= $form->field($model, 'password')
+                    ->passwordInput(['style' => 'width:380px','placeholder' => 'Password'])
+                    ->label(false)
+                ?>
 
-                <?= $form->field($model, 'email') ?>
+                <?= $form->field($model, 'repeat_password')
+                    ->passwordInput(['style' => 'width:380px','placeholder' => 'Re-enter Password'])
+                    ->label(false)
+                ?>
 
-                <?= $form->field($model, 'password')->passwordInput() ?>
+                <?= Html::submitButton('Sign up', ['class' => 'login-button', 'name' => 'signup-button']) ?>
 
-                <div class="form-group">
-                    <?= Html::submitButton('Signup', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
-                </div>
+                <?php ActiveForm::end(); ?>
+                <p class="notice-facebook">By signing up, you agree to our <?= Html::a('terms of use', ['site/index'],['class' => 'policy-a']) ?>,<?= Html::a('privacy policy', ['site/index'],['class' => 'policy-a']) ?>, and <?= Html::a('cookie policy', ['site/index'],['class' => 'policy-a']) ?>.</p>
 
-            <?php ActiveForm::end(); ?>
+                <fieldset class="title">
+                    <legend>Or</legend>
+                </fieldset>
+                <br />
+
+                <?= Html::submitButton('<i class="fa fa-facebook-square fa-lg"></i>&nbsp&nbsp&nbsp&nbsp&nbsp&nbspLog in with Facebook', ['class' => 'login-facebook', 'name' => 'login-button']) ?>
+                <p class="notice-facebook2">We'll never post anything on Facebook without your permission.</p>
+            </div>
         </div>
     </div>
 </div>
