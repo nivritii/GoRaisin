e9801d4e0cb979cd11d9dfa946e8fdc40e8bee44
cherrygu <cherrygu1209@gmail.com>
2018-01-23 14:30:19 +0800

    Div left line
    div left line

--- frontend/views/campaign/view.php
+++ frontend/views/campaign/view.php
@@ -20,7 +20,7 @@ frontend\assets\HomePageAsset::register($this);
     <!-- Main Content -->
     <div id="Content">
         <div class="content_wrapper clearfix">
-            <div class="sections_group" style="width: 63%;float: left; margin-left: 8.5%">
+            <div class="sections_group" style="width: 63%;float: left; margin-left: 8.5%;padding-right:2%;border-right: 1px solid #f0f0f0">
                 <div id=" " class="no-title no-share  post  format-standard has-post-thumbnail  category-hot-news   ">
                     <div class="section section-post-header">
                         <div class="section_wrapper clearfix">
