50fe3800f508969b5ca752c3327792e051d25d24
cherrygu <cherrygu1209@gmail.com>
2018-01-29 15:44:45 +0800

    Create campaign
    Except reward

--- frontend/views/campaign/_form_2.php
+++ frontend/views/campaign/_form_2.php
@@ -4,51 +4,41 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use dosamigos\datepicker\DatePicker;
 use frontend\models\Campaign;
-use trntv\yii\datetime\DateTimeWidget;
-use kartik\file\FileInput;
-use yii\helpers\Url;
-frontend\assets\CampaignAsset::register($this);
+use frontend\models\Reward;
 
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Campaign */
 /* @var $form yii\widgets\ActiveForm */
-$imagePath = '/'.$model->c_image;
 ?>
 
-<div class="container">
-    <h1 class="basic-title">Story</h1>
+<div class="campaignAboutYou-form">
+    <h1 class="basic-title">About you</h1>
     <br />
     <div class="form-group">
-        <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
-
-        <div style="clear:both;height: 80px">
+        <!-- <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>-->
+        <div style="width: 100%;height: 80px;margin-left: 10%;margin-right: 5%">
             <div style="float: left;display: inline-block;width: 20%">
                 <p class="item-title">Campaign video</p>
             </div>
             <div style="float: left;display: inline-block;width: 50%;margin-left: 3%">
-                <?php echo $model->c_video ?>
                 <?= $form->field($model, 'c_video')
-                    ->textInput(['style' => 'color:#940094;width:600px'])
-                    ->hint('Please upload your video to YouTube then put the link of campaign video here.')
+                    ->textInput(['maxlength' => true,'style' => 'width:600px'])
                     ->label(false)
                 ?>
             </div>
         </div>
 
-        <div style="clear:both;height: 150px">
+        <div style="width: 100%;height: 80px;margin-left: 10%;margin-right: 5%">
             <div style="float: left;display: inline-block;width: 20%">
-                <p class="item-title">Main Description</p>
+                <p class="item-title">Main description</p>
             </div>
             <div style="float: left;display: inline-block;width: 50%;margin-left: 3%">
                 <?= $form->field($model, 'c_description_long')
-                    ->textarea(['rows' => 20])
+                    ->textarea(['row' => 20,'style' => 'width:600px'])
                     ->label(false)
                 ?>
             </div>
         </div>
-
         <?php ActiveForm::end(); ?>
-
     </div>
 </div>
-
