02aca2283e24bd66924ee3e18967d095153f43bb
cherrygu <cherrygu1209@gmail.com>
2018-02-07 11:51:19 +0800

    Unit test for frontend
    Unit test for frontend

---
+++ frontend/tests/unit/models/FaqTest.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cherry
+ * Date: 07/02/2018
+ * Time: 11:46 AM
+ */
--- frontend/tests/unit/models/SignupFormTest.php
+++ frontend/tests/unit/models/SignupFormTest.php
@@ -1,10 +1,11 @@
 <?php
 namespace frontend\tests\unit\models;
 
+use \Codeception\Test\Unit;
 use common\fixtures\UserFixture;
 use frontend\models\SignupForm;
 
-class SignupFormTest extends \Codeception\Test\Unit
+class SignupFormTest extends Unit
 {
     /**
      * @var \frontend\tests\UnitTester
