cfb39d576fe73379799d7b2b8f49a0d4ab2a0079
nivritii <riti.014@gmail.com>
2018-02-09 16:46:15 +0800

    changes to fund campaign
    changes to fund campaign

--- frontend/views/campaign/fund.php
+++ frontend/views/campaign/fund.php
@@ -18,7 +18,9 @@ $model = new Campaign();
 $reward = new Reward();
 
 ?>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 <div class="container">
     <div class="col-xs-12">
@@ -29,18 +31,77 @@ $reward = new Reward();
             <!--<form>-->
             <form enctype="multipart/form-data" action="fund?id=<?=$c_id?>" method="post">
             <div class="container col-xs-12">
-                <div class="campaignAboutYou-form">
-                    <div class="form-group">
-                        <?php foreach ($rewards as $reward){?>
-                        <div class="card">
-                            <div class="card-block">
-                                <h4 class="card-title"><?=$reward->r_title?></h4>
-                                <h6 class="card-subtitle mb-2 text-muted"><input type="radio" name="reward" value="<?=$reward->r_pledge_amt?>"><?=$reward->r_description?></h6>
+                <div class="container">
+                    <div class="panel-group" id="faqAccordion" style="width: 89%">
+                        <div class="panel panel-default panel-faq">
+                            <div class="panel-heading">
+                                <a data-toggle="collapse" data-parent="#accordion-cat-1" href="#noReward">
+                                    <h4 class="panel-title">
+                                        Pleage without a reward
+                                        <span class="pull-right"><i class="glyphicon glyphicon-plus"></i></span>
+                                    </h4>
+                                </a>
+                            </div>
+                            <div id="noReward" class="panel-collapse collapse">
+                                <div class="panel-body">
+                                    <div class="form-group">
+                                    <div style="alignment:center">
+                                        <div style="display: inline-block;width: 20%">
+                                            <p class="item-title">Pledge Amount</p>
+                                        </div>
+                                        <div style="display: inline-block;margin-left: 2%;width: 50%">
+                                            <input type="text" style="width: 100%" name="reward">
+                                        </div>
+                                    </div>
+                                        <input class="btn btn-md btn-info" type="submit" value="Pledge" id="submit"  style="color: #ffffff;width: 25%">
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <?php };?>
-                        <br/>
-                        <input class="btn btn-md btn-info" type="submit" value="Pledge" id="submit" style="color: #ffffff;background-color: #940094;border: 0;width: 10%">
+                        <?php foreach ($rewards as $reward) {?>
+                        <div class="panel panel-default panel-faq">
+                            <div class="panel-heading">
+                                <a data-toggle="collapse" data-parent="#accordion-cat-1" href="#<?=$reward->r_id?>">
+                                    <h4 class="panel-title">
+                                        <input type="radio" name="reward" value="<?=$reward->r_pledge_amt?>" hidden>
+                                        <?=$reward->r_title?>
+                                        <span class="pull-right"><i class="glyphicon glyphicon-plus"></i></span>
+                                    </h4>
+                                </a>
+                            </div>
+                            <div id="<?=$reward->r_id?>" class="panel-collapse collapse">
+                                <div class="panel-body">
+                                    <table>
+                                        <tr>
+                                            <td style="width: 5%"><input type="radio" name="reward" value="<?=$reward->r_pledge_amt?>"></td>
+                                            <td style="width: 95%">
+                                                <div class="form-group">
+                                                    <div style="alignment:center">
+                                                        <div style="display: inline-block;">
+                                                            <?=$reward->r_description?>
+                                                        </div>
+                                                    </div>
+                                                    <br/>
+                                                    <div style="alignment:center">
+                                                        <div style="display: inline-block">
+                                                            <p class="item-title">Pledge Amount: </p>
+                                                        </div>
+                                                        <div style="display: inline-block">
+                                                            <input type="text" disabled value="<?=$reward->r_pledge_amt?>">
+                                                        </div>
+                                                    </div>
+                                                    <br/>
+                                                    <div style="alignment:center">
+                                                        <input class="btn btn-md btn-info" type="submit" id="submit" value="Pledge" style="color: #ffffff;width: 25%">
+                                                    </div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <?php }?>
                     </div>
                 </div>
             </div>
@@ -50,4 +111,3 @@ $reward = new Reward();
     </div>
 </div>
 
-    
