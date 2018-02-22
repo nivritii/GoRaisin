c99bef905be1488f77972bd2a51f4b494e5cd684
cherrygu <cherrygu1209@gmail.com>
2018-02-22 14:10:39 +0800

    View campaign video
    View campaign video

--- frontend/views/campaign/campaign_content.php
+++ frontend/views/campaign/campaign_content.php
@@ -10,5 +10,17 @@ use yii\widgets\ActiveForm;
 <div class="campaign-content">
     <h3>Description</h3>
     <br /><br />
+
+    <?php
+    $url = 'https://www.youtube.com/watch?v=Qu8xDIUjFUs';
+    preg_match('/[\\?\\&]v=([^\\?\\&]+)/', $url, $matches);
+    $id = $matches[1];
+    $width = '800px';
+    $height = '450px';
+?>
+    <iframe id="ytplayer" type="text/html" width="<?php echo $width ?>" height="<?php echo $height ?>"
+            src="https://www.youtube.com/embed/<?php echo $id ?>?rel=0&showinfo=0&color=white&iv_load_policy=3"
+            frameborder="0" allowfullscreen></iframe>
+    <br /><br />
     <p><?=$model->c_description_long?></p>
 </div>
